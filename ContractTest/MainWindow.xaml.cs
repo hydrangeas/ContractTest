5fa2729bc4369c9700bbb9a331ad275bb809ca9a
Akira Otaka <hydrangeas.project@gmail.com>
2019-09-23 00:14:46 +0900

    コードコントラクト追加

--- ContractTest/MainWindow.xaml.cs
+++ ContractTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +47,7 @@ namespace ContractTest
         /// <returns></returns>
         private double div(double divisor, double dividend)
         {
+            Contract.Requires(dividend != 0);
             return divisor / dividend;
         }
     }
